259c106cfeca66f4c4eb22c585046bacbc452208
Matheus Kieling <matheus.kieling@serendipe.org>
2025-08-25 16:51:24 -0300

    feat: aula 9

--- Components/GoalInput.jsx
+++ Components/GoalInput.jsx
@@ -1,6 +1,6 @@
-﻿import {Modal, StyleSheet, TextInput, View} from "react-native";
+﻿import {Modal, StyleSheet, Text, TextInput, View} from "react-native";
 import StyleButton from "./StyleButton";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function GoalInput({ onSubmit }) {
   const [input, setInput] = useState("");
@@ -10,11 +10,16 @@ export default function GoalInput({ onSubmit }) {
     setIsVisible(false);
     onSubmit(input.trim());
     setInput("");
-  }
+  };
+
+  const handleCancel = () => {
+    setIsVisible(false);
+    setInput("");
+  };
 
   return (
     <>
-      <View style={{height: 50}}>
+      <View style={{ height: 50 }}>
         <StyleButton
           title="Add new goal"
           backgroundColor={"green"}
@@ -23,35 +28,54 @@ export default function GoalInput({ onSubmit }) {
         />
       </View>
       <Modal visible={isVisible} animationType="slide">
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Your Goal"
-            value={input}
-            onChangeText={(text) => setInput(text)}
-            onSubmitEditing={handleSubmit}
-          />
-          <StyleButton
-            title="Add Goal"
-            backgroundColor={"green"}
-            color={"white"}
-            onPress={handleSubmit}
-          />
+        <View style={{justifyContent: "center", flex: 1}}>
+          <View style={{padding: 20, alignItems: "center", justifyContent: "center", }}>
+            <Text>Add your new Goal</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <View style={{height: 50}}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Your Goal"
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                onSubmitEditing={handleSubmit}
+              />
+            </View>
+            <View style={styles.actionsContainer}>
+              <View style={{flex: 2}}>
+                <StyleButton
+                  title="Add Goal"
+                  backgroundColor={"green"}
+                  color={"white"}
+                  onPress={handleCancel}
+                />
+              </View>
+              <View style={{flex: 1}}>
+                <StyleButton
+                  title="Cancel"
+                  backgroundColor={"#ccc"}
+                  color={"black"}
+                  onPress={handleCancel}
+                />
+              </View>
+            </View>
+          </View>
         </View>
       </Modal>
     </>
-
-  )
+  );
 }
 const styles = StyleSheet.create({
   inputContainer: {
-    height: 60,
-    flexDirection: "row",
-    alignItems: "center",
+    justifyContent: "center",
     gap: 10,
-    borderBottomWidth: 1,
-    borderColor: "#ccc",
-    marginBottom: 20,
+    paddingHorizontal: 20,
+  },
+  actionsContainer: {
+    flexDirection: "row",
+    gap: 10,
+    marginBottom: 15
   },
   textInput: {
     flex: 1,
@@ -59,4 +83,4 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
     paddingHorizontal: 10,
   },
-})
+});
